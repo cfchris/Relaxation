c1b9f1cd0cd1490879ec08b1355f965e2db11aac
Chris Phillips <cfchris@gmail.com>
2013-04-12 17:34:53 -0700

    Updated TryMe to populate edit fields
    I didn't want the fact that you had to re-type the values you don't want
    to change to distract people from how awesome Honey's example is.

--- Examples/Basic/TryMe/tryme.js
+++ Examples/Basic/TryMe/tryme.js
@@ -19,7 +19,7 @@ $(function() {
 		}).always(function(data){
 			for (var v in data) {
 				$('select.products').append(
-					$('<option></option>').val(data[v].ProductID).html(data[v].Name)
+					$('<option></option>').val(data[v].ProductID).html(data[v].Name).data('product',data[v])
 				);
 			}
 			if (data.length == 0) {
@@ -27,10 +27,18 @@ $(function() {
 			} else {
 				$('.noProducts').hide();
 			}
+			$('select.products').change();
 		});
 	}
 	
 	loadProducts();
+	
+	$('select.products').on('change', function() {
+		var product = $(this).find(':selected').first().data('product');
+		$('input[name=productNameSave]').val(product.Name);
+		$('input[name=priceSave]').val(product.Price);
+		$('input[name=vendorSave]').val(product.Vendor);
+	});
 	
 	$('.collapseResponse').on('click', function() {
 		$(this).closest('.response').toggle();
